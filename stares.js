45d0b086676fff963565a810a766a9a058451a5e
JNGApura <joao.apura@gmail.com>
2017-01-03 19:11:29 -0800

    Revert "Merge remote-tracking branch 'origin/horizontal_version'"
    This reverts commit 719e7e0163abd3f979e86ae99299a99925b531b5, reversing
    changes made to 4abd68b671f4e149a0b038274f7ffa5386cf6af3.

---
+++ stares.js
@@ -0,0 +1,34 @@
+/*function member(line_value, column_value){
+    this.line = line_value;
+    this.column = column_value;
+}
+
+function initialize_team(){
+
+    team_size=27;
+
+    var team = new Array();
+    var i;
+
+    for(i=0 ; i<team_size ; i++)
+    {
+        team[i] = new member());
+    }
+};
+
+function array_print(){
+
+    team[1]= member1;
+  
+
+    team[0] = member0:
+
+
+    document.getElementById("demo").innerHTML = "ola";
+
+} */
+
+function myFunction() {
+   document.getElementById("demo").innerHTML = "ola";
+}
+
